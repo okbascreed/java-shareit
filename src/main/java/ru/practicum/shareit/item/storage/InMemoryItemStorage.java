6925dab53b6ffef61388d32faa8eb0bd73320ab6
Jack <okbascreed@yandex.ru>
2023-04-07 19:57:11 +0300

    feature<>: checkstyle issues fixed;

--- src/main/java/ru/practicum/shareit/item/storage/InMemoryItemStorage.java
+++ src/main/java/ru/practicum/shareit/item/storage/InMemoryItemStorage.java
@@ -10,6 +10,7 @@ import java.util.Map;
 
 @Component
 public class InMemoryItemStorage implements ItemStorage {
+
     private final Map<Long, Item> items = new HashMap<>();
 
     private Long itemId = 0L;
